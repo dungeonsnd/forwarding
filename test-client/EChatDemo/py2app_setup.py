8141468c1e86af8586831a3c3161bfce0e9389fa
jeffery <dungeonsnd@gmail.com>
2016-01-29 22:17:57 +0800

    add ntplib to packcage

--- test-client/EChatDemo/py2app_setup.py
+++ test-client/EChatDemo/py2app_setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 APP = ['EChat.py']
 DATA_FILES = ['DlgChat.ui']
-OPTIONS = {'argv_emulation': False, 'iconfile':'ls.ico', 'includes':['sip', 'PyQt4', 'PyQt4.QtGui', 'PyQt4.QtCore', 'PyQt4.uic', 'sitecustomize']}
+OPTIONS = {'argv_emulation': False, 'iconfile':'ls.ico', 'includes':['ntplib','sip', 'PyQt4', 'PyQt4.QtGui', 'PyQt4.QtCore', 'PyQt4.uic', 'sitecustomize']}
 setup(
     app=APP,
     data_files=DATA_FILES,
